9c6e9a80121d15f41db87b6b3fd32776efa5e061
MUGISHA-Pascal <mugishapascal2008@gmail.com>
2024-10-01 21:04:11 +0200

    adding essentials

---
+++ src/card.ts
@@ -0,0 +1,24 @@
+import chalk from "chalk";
+import boxen from "boxen";
+
+const email = chalk.bold.blue("mugishapascal@gmail.com");
+const number = chalk.bold.blue("+250786493844");
+const github = chalk.bold.yellow("https://github.com/MUGISHA-Pascal");
+const linkedin = chalk.bold.yellow("www.linkedin.com/in/mugisha-pascal");
+const role = chalk.bold.yellow("Software engineer");
+
+const output = `
+            MUGISHA Pascal
+${chalk.bold.green("role")}         : ${role} 
+${chalk.bold.green("phone number")} : ${number} 
+${chalk.bold.green("email")}        : ${email} 
+${chalk.bold.green("linkedin")}     : ${linkedin} 
+${chalk.bold.green("github")}       : ${github} 
+`;
+const frameOutput = boxen(output, {
+  padding: 1,
+  borderStyle: "round",
+  margin: 1,
+  borderColor: "green",
+});
+console.log(frameOutput);
