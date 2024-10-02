9364a1e284c8137d8491159af4a2ec0359caccee
MUGISHA-Pascal <mugishapascal2008@gmail.com>
2024-10-02 13:16:57 +0200

    updating

--- dist/card.js
+++ dist/card.js
@@ -1,19 +1,14 @@
+#!/usr/bin/env node
 import chalk from "chalk";
 import boxen from "boxen";
-const email = chalk.bold.blue("mugishapascal@gmail.com");
-const number = chalk.bold.blue("+250786493844");
-const github = chalk.bold.yellow("https://github.com/MUGISHA-Pascal");
-const linkedin = chalk.bold.yellow("www.linkedin.com/in/mugisha-pascal");
-const role = chalk.bold.yellow("Software engineer");
-const name = chalk.bold("MUGISHA Pascal");
-const output = `               ${name}
-${chalk.bold.green("role")}         : ${role} 
-${chalk.bold.green("phone number")} : ${number} 
-${chalk.bold.green("email")}        : ${email} 
-${chalk.bold.green("linkedin")}     : ${linkedin} 
-${chalk.bold.green("github")}       : ${github} 
-`;
-const frameOutput = boxen(output, {
+var email = chalk.bold.blue("mugishapascal@gmail.com");
+var number = chalk.bold.blue("+250786493844");
+var github = chalk.bold.yellow("https://github.com/MUGISHA-Pascal");
+var linkedin = chalk.bold.yellow("www.linkedin.com/in/mugisha-pascal");
+var role = chalk.bold.yellow("Software engineer");
+var name = chalk.bold("MUGISHA Pascal");
+var output = "               ".concat(name, "\n").concat(chalk.bold.green("role"), "         : ").concat(role, " \n").concat(chalk.bold.green("phone number"), " : ").concat(number, " \n").concat(chalk.bold.green("email"), "        : ").concat(email, " \n").concat(chalk.bold.green("linkedin"), "     : ").concat(linkedin, " \n").concat(chalk.bold.green("github"), "       : ").concat(github, " \n");
+var frameOutput = boxen(output, {
     padding: 1,
     borderStyle: "round",
     margin: 1,
